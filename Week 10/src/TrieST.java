3cf5fa7ca2df181463afaf91c4832b6c61125ae2
陈响 <chenxiang@chenxiangdeMacBook-Pro.local>
2020-07-01 01:33:31 -0700

    update

---
+++ Week 10/src/TrieST.java
@@ -0,0 +1,36 @@
+public class TrieST<Value> {
+    private static final int R = 256; //extended ASCII
+    private Node root = new Node();
+    private static class Node{
+        private Object value;       //use Object instead of Value since no generic array creation in Java
+        private Node[] next = new Node[R];  //R代表基数，类似26个字母，就是26，可以立刻找到对应的字母（A->0,B->1,..）
+    }
+    //create an empty symbol table
+    public TrieST(){
+
+    }
+    //put Key-value into the symbol table
+    public void put(String key, Value val){
+        root = put(root,key,val,0);
+    }
+    private Node put(Node x, String key, Value val, int d){
+        if (x == null){
+            x = new Node();
+        }
+        if (d == key.length()){
+            x.value = val;
+            return x;
+        }
+        char c = key.charAt(d);         //第d个字符对应的关键字
+        x.next[c] = put(x.next[c],key,val,d + 1);
+        return x;
+    }
+    //return value paired with given key
+    public void get(String key){
+
+    }
+    //delete key and corresponding value
+    public void delete(String key){
+
+    }
+}
